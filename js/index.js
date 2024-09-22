1906365485148a2778e0a9dfae0b0ff90b2f34ce
SifatSararChistee <sifatsararchistee@gamil.com>
2024-09-22 10:58:02 +0600

    added first section features

--- js/index.js
+++ js/index.js
@@ -6,5 +6,26 @@ document.getElementById("calculate").addEventListener("click", function () {
 
   const expenses = software + courses + internet;
   const availableBalance = income - expenses;
-  console.log(availableBalance);
+  document.getElementById("total-expenses").innerText = expenses;
+  document.getElementById("balance").innerText = availableBalance;
+
+  document.getElementById("results").classList.remove("hidden");
 });
+
+document
+  .getElementById("calculate-savings")
+  .addEventListener("click", function () {
+    const income = inputFieldValue("income");
+    const software = inputFieldValue("software");
+    const courses = inputFieldValue("courses");
+    const internet = inputFieldValue("internet");
+    const savings = inputFieldValue("savings");
+
+    const expenses = software + courses + internet;
+    const availableBalance = income - expenses;
+    const savingsAmount = (availableBalance * savings) / 100;
+    const remainingBalance = availableBalance - savingsAmount;
+
+    document.getElementById("savings-amount").innerText = savingsAmount;
+    document.getElementById("remaining-balance").innerText = remainingBalance;
+  });
