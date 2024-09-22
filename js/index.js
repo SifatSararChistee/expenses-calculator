b2211223bcf8e0706cc2015d063a4531bb52757d
SifatSararChistee <sifatsararchistee@gamil.com>
2024-09-22 12:02:15 +0600

    added all features

--- js/index.js
+++ js/index.js
@@ -3,13 +3,18 @@ document.getElementById("calculate").addEventListener("click", function () {
   const software = inputFieldValue("software");
   const courses = inputFieldValue("courses");
   const internet = inputFieldValue("internet");
-
   const expenses = software + courses + internet;
   const availableBalance = income - expenses;
   document.getElementById("total-expenses").innerText = expenses;
   document.getElementById("balance").innerText = availableBalance;
-
   document.getElementById("results").classList.remove("hidden");
+  const historyItem = document.createElement("div");
+  historyItem.innerHTML = `
+  <p> ${new Date().toDateString()}</p>
+  <p>Income ${income}</p>
+  <p>Expenses ${expenses}</p>
+  <p>Balance ${availableBalance}</p>`;
+  document.getElementById("history-list").appendChild(historyItem);
 });
 
 document
@@ -29,3 +34,46 @@ document
     document.getElementById("savings-amount").innerText = savingsAmount;
     document.getElementById("remaining-balance").innerText = remainingBalance;
   });
+
+document.getElementById("history-tab").addEventListener("click", function () {
+  document
+    .getElementById("history-tab")
+    .classList.add(
+      "text-white",
+      "bg-gradient-to-r",
+      "from-blue-500",
+      "to-purple-600"
+    );
+  document
+    .getElementById("assistant-tab")
+    .classList.remove(
+      "bg-gradient-to-r",
+      "from-blue-500",
+      "to-purple-600",
+      "text-white"
+    );
+  document.getElementById("assistant-tab").classList.add("text-gray-600");
+  document.getElementById("expense-form").classList.add("hidden");
+  document.getElementById("history-section").classList.remove("hidden");
+});
+
+document.getElementById("assistant-tab").addEventListener("click", function () {
+  document.getElementById("expense-form").classList.remove("hidden");
+  document
+    .getElementById("assistant-tab")
+    .classList.add(
+      "text-white",
+      "bg-gradient-to-r",
+      "from-blue-500",
+      "to-purple-600"
+    );
+  document
+    .getElementById("history-tab")
+    .classList.remove(
+      "bg-gradient-to-r",
+      "from-blue-500",
+      "to-purple-600",
+      "text-white"
+    );
+  document.getElementById("history-section").classList.add("hidden");
+});
